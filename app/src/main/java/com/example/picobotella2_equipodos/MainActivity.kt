eb702cc69411faae16245eb0bb975c8351d058c3
Jaider-Dev <jaiderbermudez.contact@gmail.com>
2024-12-03 09:55:24 -0500

    carga de res

---
+++ app/src/main/java/com/example/picobotella2_equipodos/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.picobotella2_equipodos
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val bottleIcon: ImageView = findViewById(R.id.bottleIcon)
+
+        // Carga la animación de rotación
+        val rotateAnimation = AnimationUtils.loadAnimation(this, R.anim.animated_bottle)
+
+        // Inicia la animación
+        bottleIcon.startAnimation(rotateAnimation)
+    }
+}
+
