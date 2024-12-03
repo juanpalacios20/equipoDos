a03412282b89d9cf87b7b5a9c39fdf4f21f532f6
Jaider-Dev <jaiderbermudez.contact@gmail.com>
2024-12-03 09:29:46 -0500

    estructuración del proyecto Imagen de botella Musica Fondo del home principal

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,14 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.dagger.hilt.android") version "2.47" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.24-1.0.20")
+    }
 }
